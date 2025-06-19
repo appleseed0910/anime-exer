36d28fbcedec9e9f155891cd96b9aaed7252931f
appleseed0910 <appleseed0910@gmail.com>
2025-06-19 21:53:34 +0800

    add files

--- anime-demo/src/main.js
+++ anime-demo/src/main.js
@@ -3,25 +3,39 @@ import javascriptLogo from "./javascript.svg";
 import viteLogo from "/vite.svg";
 import { setupCounter } from "./counter.js";
 import  * as anime from '../node_modules/animejs/lib/anime.esm.js'
+import { waapi } from "animejs";
 
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vite.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
 
-console.log(anime);
-const el = anime.utils.$("#myDiv");
-console.log(el);
+const [ $logo ] = anime.utils.$('.logo.js');
+const [ $button ] = anime.utils.$('button');
+let rotations = 0;
+
+anime.animate('.logo.js', {
+    scale: [
+      { to: 1.25, ease: 'inOut(3)', duration: 200 },
+      { to: 1, ease: anime.createSpring({ stiffness: 300 }) }
+    ],
+    loop: true,
+    loopDelay: 300,
+  });
+  
+  // Make the logo draggable around its center
+  anime.createDraggable('.logo.js', {
+    container: [0, 0, 0, 0],
+    releaseEase: anime.createSpring({ stiffness: 200 })
+  });
+  
+  // Animate logo rotation on click
+  const rotateLogo = () => {
+    rotations++;
+    $button.innerText = `rotations: ${rotations}`;
+    anime.animate($logo, {
+      rotate: rotations * 90,
+      ease: 'out(4)',
+      duration: 200,
+    });
+  }
+  
+
+  
+  $button.addEventListener('click', rotateLogo);
