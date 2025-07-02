105035bf3d80bf399fd0a7d9c2ba3d7d58c5d024
appleseed0910 <xinyi.hu01@sap.com>
2025-07-02 18:02:28 +0800

    add files and logic

---
+++ anime-demo/src/demo.js
@@ -0,0 +1,6 @@
+import "./style.css";
+import javascriptLogo from "./javascript.svg";
+import viteLogo from "/vite.svg";
+import { setupCounter } from "./counter.js";
+import { Timeline, onScroll, animate } from "animejs";
+import { waapi } from "animejs";
