1ef2314ff4bbc5ba7b309f475f0a154128806c2e
appleseed0910 <xinyi.hu01@sap.com>
2025-07-03 18:55:35 +0800

    opening done

--- anime-demo/src/demo.js
+++ anime-demo/src/demo.js
@@ -2,5 +2,8 @@ import "./style.css";
 import javascriptLogo from "./javascript.svg";
 import viteLogo from "/vite.svg";
 import { setupCounter } from "./counter.js";
-import { Timeline, onScroll, animate } from "animejs";
+import { Timeline, onScroll, animate, utils } from "animejs";
 import { waapi } from "animejs";
+
+const [$scroll] = utils.$(".scroll");
+
