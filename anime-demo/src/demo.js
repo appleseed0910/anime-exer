393da53d165bc8dcb9f0c93e61782fe9151e0612
appleseed0910 <xinyi.hu01@sap.com>
2025-07-11 22:16:56 +0800

    add files 2nd version!!!!yahoooo!!!!

--- anime-demo/src/demo.js
+++ anime-demo/src/demo.js
@@ -5,5 +5,29 @@ import { setupCounter } from "./counter.js";
 import { Timeline, onScroll, animate, utils } from "animejs";
 import { waapi } from "animejs";
 
-const [$scroll] = utils.$(".scroll");
+
+const sections = document.querySelectorAll('section');
+
+let currentIdx = 0;
+let isScrolling = false;
+
+function scrollToSection(idx) {
+    if (idx < 0 || idx >= sections.length) return;
+
+    isScrolling = true;
+    sections[idx].scrollIntoView({behavior: 'smooth'});
+    currentIdx = idx;
+
+    setTimeout(() => isScrolling = false, 800)
+};
+
+window.addEventListener('wheel', (e) => {
+    if (isScrolling) return;
+
+    if (e.deltaY > 0) {
+        scrollToSection(currentIdx + 1);
+    } else {
+        scrollToSection(currentIdx -1);
+    }
+})
 
