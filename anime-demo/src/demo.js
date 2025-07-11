311f4e09a6f0d469329e0291e3cc9b868d3833e9
appleseed0910 <appleseed0910@gmail.com>
2025-07-12 00:22:51 +0800

    shit vite shit

--- anime-demo/src/demo.js
+++ anime-demo/src/demo.js
@@ -2,15 +2,21 @@ import "./style.css";
 import javascriptLogo from "./javascript.svg";
 import viteLogo from "/vite.svg";
 import { setupCounter } from "./counter.js";
-import { Timeline, onScroll, animate, utils } from "animejs";
-import { waapi } from "animejs";
+// import anime from '../node_modules/animejs/lib/anime.esm.js';
+// import  * as anime from '../node_modules/animejs/lib/anime.esm.js'
+// import { animate } from "animejs";
+// import anime from 'animejs';
+// import * as anime from 'animejs';
 
+// import anime from 'animejs/lib/anime.esm.js';
 
 const sections = document.querySelectorAll('section');
 
 let currentIdx = 0;
 let isScrolling = false;
 
+
+// smooth scroll fn
 function scrollToSection(idx) {
     if (idx < 0 || idx >= sections.length) return;
 
@@ -21,11 +27,17 @@ function scrollToSection(idx) {
     setTimeout(() => isScrolling = false, 800)
 };
 
+
+// smooth scroll event
 window.addEventListener('wheel', (e) => {
     if (isScrolling) return;
 
     if (e.deltaY > 0) {
         scrollToSection(currentIdx + 1);
+        anime({
+            targets: ".blocks",
+            translateX: 200
+        })
     } else {
         scrollToSection(currentIdx -1);
     }
